c7217216a13352a699e8009d434b4a80d0bed4bd
BITU KUMAR <125851784+bitu626@users.noreply.github.com>
2023-03-15 11:57:21 +0530

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,10 @@
+let a = parseInt(prompt("Enter first no:"));
+let b = parseInt(prompt("Enter second no:"));
+
+function Add(c, d) {
+    document.write(a + b);
+}
+
+function Sub(e, f) {
+    document.write(a - b);
+}
